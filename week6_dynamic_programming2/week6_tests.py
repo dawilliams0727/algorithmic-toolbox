531226623b3d1314c22067ee5661c6887c8dac04
David Williams <109742173+phenomenon1995@users.noreply.github.com>
2025-03-16 12:08:58 -0400

    Setup solutions and Tests

---
+++ week6_dynamic_programming2/week6_tests.py
@@ -0,0 +1,15 @@
+import unittest
+from random import randint
+from knapsack import maximum_gold
+from partition3 import partition3
+from placing_parentheses import evaluate
+
+class TestKnapsack(unittest.TestCase):
+    ...
+class TestPartition3(unittest.TestCase):
+    ...
+class TestPlacingParenthesis(unittest.TestCase):
+    ...
+
+if __name__ == "__main__":
+    unittest.main()

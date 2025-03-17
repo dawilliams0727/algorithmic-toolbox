0aa853939b74fbfe22a866e1403fc5512600d82a
David Williams <109742173+phenomenon1995@users.noreply.github.com>
2025-03-16 23:35:45 -0400

    completed assignments

--- week6_dynamic_programming2/knapsack.py
+++ week6_dynamic_programming2/knapsack.py
@@ -2,7 +2,32 @@ from sys import stdin
 
 
 def maximum_gold(capacity, weights):
-    assert False
+    """
+        Given a set of gold bars of various weights and
+        a backpack that can hold at most W pounds,
+        place as much gold as possible into the backpack.
+    """
+    # initialize DP table with item count rows, and capacity length columns
+    value = [[0]*(capacity+1) for _ in range(len(weights)+1)]
+    # for each gold bar
+    for i in range(1,len(weights)+1):
+        goldBar = weights[i-1]
+        # for each smaller capacity up to target capacity
+        for w in range(capacity+1):
+            # if the weight of goldBar is at most weight of backpack
+            # set value of backpack as if gold bar isn't used
+            value[i][w] = value[i-1][w]
+            if goldBar <= w:
+                # weight if gold bar is used
+                withBar = value[i-1][w-goldBar] + goldBar
+                # value[goldBar][w] is the larger of the two
+                value[i][w] = max(withBar, value[i][w])
+
+    return value[-1][-1]
+
+
+    
+
 
 
 if __name__ == '__main__':
--- week6_dynamic_programming2/placing_parentheses.py
+++ week6_dynamic_programming2/placing_parentheses.py
@@ -10,8 +10,44 @@ def evaluate(a, b, op):
 
 
 def maximum_value(dataset):
-    # write your code here
-    return 0
+    """
+    Parenthesize an arithmetic expression to maximize its value and return the value of the expression
+    """
+    # split dataset into digits and operands
+    digits = [int(dataset[i]) for i in range(0,len(dataset), 2)]
+    operands = [dataset[i] for i in range(1, len(dataset),2)] 
+    # determine n
+    n = (len(dataset) + 1) // 2
+    # min and max table to store values for E(i,j) of size
+    min_table = [[float("inf") for _ in range(n)] for _ in range(n)]
+    max_table = [[float("-inf") for _ in range(n)] for _ in range(n)]
+    # initialize the main diagonals with the digits (corresponds to no need for evaluation of min or max)
+    for i in range(n):
+        min_table[i][i] = digits[i]
+        max_table[i][i] = digits[i]
+    
+    # define function for calulating min and max
+    def MinAndMax(i: int,j: int):
+        """
+        Calculate the minimum and maximum values for all possible subexpressions ranging from i to j
+        """
+        minValue, maxValue = float("inf"), float("-inf")
+        for k in range(i, j):
+            a = evaluate(max_table[i][k], max_table[k+1][j], operands[k])
+            b = evaluate(min_table[i][k], min_table[k+1][j], operands[k])
+            c = evaluate(max_table[i][k], min_table[k+1][j], operands[k])
+            d = evaluate(min_table[i][k], max_table[k+1][j], operands[k])
+            minValue = min(minValue,a,b,c,d)
+            maxValue = max(maxValue,a,b,c,d)
+        return (minValue, maxValue)
+    
+    # iterate along each diagonal, filling dp tables
+    for s in range(1, n):
+        for i in range(n-s):
+            j = i + s
+            min_table[i][j], max_table[i][j] = MinAndMax(i,j)
+
+    return max_table[0][-1]
 
 
 if __name__ == "__main__":
